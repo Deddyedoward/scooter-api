9ce4dc5d69257b6d661bbbb184b05952dd10f17a
deddy edoward <deddy.edoward@yahoo.com>
2024-10-02 16:30:21 +0700

    feat: adding user module, adjustment auth module and database module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-    controllers: [AuthController],
-    providers: [AuthService],
-    exports: [AuthService]
+  imports: [UserModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
