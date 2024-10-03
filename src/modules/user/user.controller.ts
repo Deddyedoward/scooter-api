50d034a8e34277ca7c2611ac589c423a032238ca
deddy edoward <deddy.edoward@yahoo.com>
2024-10-03 16:28:42 +0700

    feat: add all users endpoint in user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req, Res } from "@nestjs/common";
+import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController {
+    constructor(
+        private userService: UserService
+    ) {}
     
+    @Get()
+    async index() {
+        return await this.userService.findAll()
+    }
 }
