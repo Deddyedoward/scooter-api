4e80a3d6cd2c525c7a5c5821b81c2902fe096b9b
deddy edoward <deddy.edoward@yahoo.com>
2024-10-06 20:17:31 +0700

    feat: add ride schema database

---
+++ src/modules/ride/schema/ride.schema.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+export const RideSchema = new mongoose.Schema({
+    scooterId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Scooter', required: true},
+    startTime: { type: Date },
+    endTime: { type: Date },
+    startLocation: { 
+        type: { type: String, default: "Point" },
+        coordinates: { type: [Number], required: true }
+    },
+    endLocation: { 
+        type: { type: String, default: "Point" },
+        coordinates: { type: [Number], required: true }
+    },
+    distance: { type: Number },
+    fare: { type: String },
+    status: { type: String, enum: ['active', 'completed', 'canceled'], default: 'active'}
+}, { timestamps: true })
