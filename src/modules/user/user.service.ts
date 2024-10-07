84aadcf6b27d65fcd45c1d22f8fadd3fb5132992
deddy edoward <deddy.edoward@yahoo.com>
2024-10-07 21:05:50 +0700

    fix: restore methods in user services

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Model } from "mongoose";
 import { GetUserDto } from "./dto/get-user.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UserService {
@@ -11,5 +12,17 @@ export class UserService {
 
     async findAll() {
         return await this.userModel.find();
+    }
+
+    async findOne() {
+        return await this.userModel.findOne().exec();
+    }
+
+    async userRegistration(payload: CreateUserDto) {
+        return await this.userModel.create(payload);
+    }
+
+    async findExistUserByEmail(email: String) {
+        return await this.userModel.findOne({ email: email}).exec();
     }
 }
